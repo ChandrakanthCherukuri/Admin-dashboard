289422f8b5ff743d5715c7929464470cee52237a
Chandrakanth Cherukuri <119747587+ChandrakanthCherukuri@users.noreply.github.com>
2025-06-14 20:55:42 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 // src/index.js
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // if you have Tailwind or global styles
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
 );
